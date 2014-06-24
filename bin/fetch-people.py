1b52c799d7f06c5ba5ae4e57fb6ea9c802808b6c
Matthew Somerville <matthew@mysociety.org>
2014-06-24 18:39:13 +0100

    Initial scripts to fetch/import people/committees.

---
+++ bin/fetch-people.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+import json
+import os
+import re
+import unicodedata
+
+import bs4
+import requests
+import requests_cache
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'sayit_philadelphia.settings'
+
+BASE_DIR = os.path.dirname(__file__)
+CACHE_DIR = os.path.join(BASE_DIR, 'data')
+requests_cache.install_cache(CACHE_DIR)
+
+def slugify(value):
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub('[^\w\s-]', '', value).strip().lower()
+    return re.sub('[-\s]+', '-', value)
+
+people = {}
+organizations = {}
+memberships = []
+
+def get_person(link):
+    url = link['href']
+    name = ' '.join(link(text=True)).strip().replace(u'\u2019', "'")
+    if url not in people:
+        m = re.search('(?: |-)([^ -]+?)(?:, Jr\.)?$', name)
+        family_name = m.group(1)
+        if family_name == 'Brown': family_name = 'Reynolds Brown'
+        people[url] = {
+            'id': slugify(name),
+            'name': name,
+            'family_name': family_name,
+            'links': [
+                { 'url': url, 'note': 'Official site' }
+            ]
+        }
+    return people[url]['id']
+
+def get_org(link):
+    name = link.string
+    if name not in organizations:
+        organizations[name] = {
+            'id': slugify(name),
+            'name': name,
+            'classification': 'Committee',
+            'links': [
+                { 'url': link['href'], 'note': 'Official site' }
+            ]
+        }
+    return organizations[name]
+
+def add_membership(role, org, link):
+    memberships.append({
+        'label': role + ' of ' + org['name'],
+        'role': role,
+        'person_id': get_person(link),
+        'organization_id': org['id'],
+    })
+
+soup = bs4.BeautifulSoup(requests.get('http://philadelphiacitycouncil.net/standing-committees/').text)
+committees = soup.find(id='menu-standing-committees').find_all('a')
+for link in committees:
+    soup = bs4.BeautifulSoup(requests.get(link['href']).text)
+    comm = get_org(link)
+
+    content = soup.find(class_='post-entry')
+    for el in content.contents:
+        if isinstance(el, bs4.NavigableString):
+            # Ignore whitespace
+            if not re.match('^\s*$', el):
+                raise Exception, el
+        elif el.name == 'h3':
+            hdg = el.string
+        elif el.name == 'p' and hdg == 'Responsiblity':
+            summary = ' '.join(el(text=True))
+            summary = comm.get('summary', '') + '\n\n' + summary
+            comm['summary'] = summary.strip()
+        elif el.name == 'p' and hdg in ('Chair', 'Vice Chair'):
+            links = el('a')
+            assert len(links) == (2 if 'Public Health' in comm['name'] and hdg == 'Vice Chair' else 1), (hdg, el)
+            for link in links:
+                add_membership(hdg, comm, link)
+        elif el.name == 'p':
+            if hdg == 'Hearing Materials': continue
+            assert hdg == 'Members', hdg
+            if el.string and not el.a:
+                assert re.search('(?i)Consisting of (all|not less than five \(5\)) members', el.string), el.string
+            else:
+                links = el('a')
+                links = [ l for l in links if l.string != '.' ]
+                if el.string:
+                    assert len(links) == 1
+                else:
+                    assert len(links) >= 4, (hdg, len(links))
+                for link in links:
+                    add_membership('Member', comm, link)
+        else:
+            raise Exception, el
+
+out = {
+    'persons': people.values(),
+    'organizations': organizations.values(),
+    'memberships': memberships,
+}
+print json.dumps(out, separators=(',', ': '), indent=2, sort_keys=True)
---
+++ bin/import-people.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import json
+import os
+import re
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'sayit_philadelphia.settings'
+
+from speeches.models import Speaker
+from popolo.models import Organization, Membership
+from instances.models import Instance
+
+data = json.load(open(sys.argv[1]))
+instance = Instance.objects.get(label='default')
+
+for o in data['organizations']:
+    org, created = Organization.objects.get_or_create(
+        identifiers__identifier=o['id'],
+        defaults={
+            'name': o['name'],
+            'classification': o['classification'],
+        }
+    )
+    if created:
+        org.identifiers.create(
+            identifier=o['id'],
+            scheme='JSON',
+        )
+        org.links.create(
+            url=o['links'][0]['url'],
+            note=o['links'][0]['note'],
+        )
+
+for person in data['persons']:
+    spkr, created = Speaker.objects.get_or_create(
+        instance=instance,
+        identifiers__identifier=person['id'],
+        defaults={
+            'name': person['name'],
+            'family_name': person['family_name']
+        }
+    )
+    if created:
+        spkr.identifiers.create(
+            identifier=person['id'],
+            scheme='JSON',
+        )
+        spkr.links.create(
+            url=person['links'][0]['url'],
+            note=person['links'][0]['note'],
+        )
+
+for mship in data['memberships']:
+    person = Speaker.objects.get(identifiers__identifier=mship['person_id'])
+    org = Organization.objects.get(identifiers__identifier=mship['organization_id'])
+    Membership.objects.get_or_create(
+        label=mship['label'],
+        role=mship['role'],
+        person=person,
+        organization=org,
+    )
